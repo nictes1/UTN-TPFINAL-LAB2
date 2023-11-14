5bd85777f429055a91e56c72570115607e661596
fedesosa4848 <federico.sosa4848@gmail.com>
2023-11-14 16:22:04 -0300

    Prototipo de menu y funcion de alquiler

--- main.c
+++ main.c
@@ -12,19 +12,27 @@ const char * archivoLectores = "archivoLectores.bin";
 const char * archivoLibros = "archivoLibros.bin";
 const char * archivoAlquileres = "archivoAlquileres.bin";
 
+void mostrarMenuBibliotecario(listaGeneros ** listaDeGeneros, nodoLector ** listaDeLectores, nodoAlquiler ** listaDeAlquileres);
+void mostrarMenuAlquiler(listaGeneros ** listaDeGeneros, nodoLector ** listaDeLectores, nodoAlquiler ** listaDeAlquileres);
+
 int main()
 {
+        /*
         //Funcion para cargar archivos
 
         //cargarArchivoLectores(archivoLectores);
         //cargarLibroEnArchivo(archivoLibros);
 
         //Funcion para mostrar archivos;
+
         //mostrarArchivolectores(archivoLectores);
         // mostrarArchivoDeLibros(archivoLibros);
+        //mostrarArchivoAlquileres(archivoAlquileres);
 
-        //Inicializar la lista
+        //Inicializar la lista lectores
+
         nodoLector * listadoDeLectores = iniciarLista();
+
         //Funcion para cargar la lista con los datos del archivo
         listadoDeLectores = cargarLectoresDesdeArchivo (archivoLectores);
         puts("\n");
@@ -35,21 +43,186 @@ int main()
         puts("Nodo a buscar \n");
         mostrarLector(aBuscar->info);
 
+
+        //Funciones de libros
+
+
          puts("Libros\n");
 
          puts("\n");
-         listaGeneros * listaPrincipal = inicializarListaGeneros ();
-         listaPrincipal = cargarListaDeGenerosDesdeArchivo(archivoLibros,listaPrincipal);
-         recorrerListaDeGeneros(listaPrincipal);
+         listaGeneros * listaPrincipal = inicializarListaGeneros (); //inicializar La lista
+         listaPrincipal = cargarListaDeGenerosDesdeArchivo(archivoLibros,listaPrincipal); //Cargar la lista con los datos del archivo
+         recorrerListaDeGeneros(listaPrincipal); //mostrar La lista Completa
 
-         nodoAlquiler * listaAlquileres = inicializarListaAlquiler();
+         puts("Libro a buscar\n");  // Buscar un libro por titulo
+         char nombreLibro [30];
+         fflush(stdin);
+         gets(nombreLibro);
+
+         puts("Mostrar por genero\n");
+
+        mostrarLibrosPorGenero(listaPrincipal,"Edad Media");
+
+        puts("Datos de un libro");
+        //Mostrar un libro
+
+         nodoArbolLibro *libroAbuscar = buscarLibroPorTituloEnLista(listaPrincipal, nombreLibro);
+
+
+         if(libroAbuscar != NULL)
+         {
+             puts("\nDato del libro\n");
+
+             mostrarLibro(libroAbuscar->dato);
+         }else
+         {
+             puts("Libro inexistente");
+         }
+
+
+        //Funciones de alquiler
+
+        //Inicializar la lista y cargarla con los datos del archivo de alquiler
+            nodoAlquiler *listaAlquileres = cargarAlquileresDesdeArchivo(archivoAlquileres);
+
+        //mostrarLa lista de alquileres
+            mostrarAlquileres(listaAlquileres);
 
          realizarAlquiler(archivoLectores,archivoLibros,archivoAlquileres,&listaPrincipal,&listadoDeLectores,&listaAlquileres);
+        */
+
+        //Funcion para cargar la lista con los datos del archivo
+        nodoLector * listadoDeLectores = iniciarLista();
+        listadoDeLectores = cargarLectoresDesdeArchivo (archivoLectores);
+
+        //
+        listaGeneros * listaPrincipal = inicializarListaGeneros (); //inicializar La lista
+        listaPrincipal = cargarListaDeGenerosDesdeArchivo(archivoLibros,listaPrincipal); //Cargar la lista con los datos del archivo
+
+        //Inicializar la lista y cargarla con los datos del archivo de alquiler
+        nodoAlquiler *listaAlquileres = cargarAlquileresDesdeArchivo(archivoAlquileres);
+
+         int opcion;
+
+    do {
+        // Mostrar menú principal
+        printf("\n=== Menú Principal ===\n");
+        printf("1. Acciones del Bibliotecario\n");
+        printf("2. Acciones de Alquiler\n");
+        printf("0. Salir\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                mostrarMenuBibliotecario(&listaPrincipal,&listadoDeLectores,&listaAlquileres);
+                break;
+            case 2:
+                mostrarMenuAlquiler(&listaPrincipal,&listadoDeLectores,&listaAlquileres);
+                break;
+            case 0:
+                printf("Saliendo del programa. ¡Hasta luego!\n");
+                break;
+            default:
+                printf("Opción no válida. Intente nuevamente.\n");
+                break;
+        }
+
+    } while (opcion != 0);
+
+    // ... Resto de tu código ...
 
     return 0;
 }
 
+void mostrarMenuBibliotecario(listaGeneros ** listaDeGeneros, nodoLector ** listaDeLectores, nodoAlquiler ** listaDeAlquileres) {
+    int opcion;
 
+    do {
+        // Mostrar menú del Bibliotecario
+        printf("\n=== Menú del Bibliotecario ===\n");
+        printf("1. Agregar nuevo libro\n");
+        printf("2. Mostrar lista de libros por género\n");
+        printf("3. Mostrar información de un libro por título\n");
+        printf("0. Volver al menú principal\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                agregarLibroAListaYArchivo(listaDeGeneros, archivoLibros);
+                break;
+            case 2:
+                //
+                puts("Mostrar por genero\n");
+                mostrarLibrosPorGenero(listaDeGeneros,"Edad Media");
+                break;
+            case 3:
+                //
+                puts("Datos de un libro");
+                //Mostrar un libro
+                 puts("Libro a buscar\n");  // Buscar un libro por titulo
+                char nombreLibro [30];
+                fflush(stdin);
+                gets(nombreLibro);
+
+                nodoArbolLibro *libroAbuscar = buscarLibroPorTituloEnLista(listaDeGeneros, nombreLibro);
+
+
+                if(libroAbuscar != NULL)
+                {
+                    puts("\nDato del libro\n");
+
+                    mostrarLibro(libroAbuscar->dato);
+                }else
+                {
+                puts("Libro inexistente");
+                }
+                break;
+            case 0:
+                printf("Volviendo al menú principal.\n");
+                break;
+            default:
+                printf("Opción no válida. Intente nuevamente.\n");
+                break;
+        }
+
+    } while (opcion != 0);
+}
+
+void mostrarMenuAlquiler(listaGeneros ** listaDeGeneros, nodoLector ** listaDeLectores, nodoAlquiler ** listaDeAlquileres) {
+    int opcion;
+
+    do {
+        // Mostrar menú de Alquiler
+        printf("\n=== Menú de Alquiler ===\n");
+        printf("1. Realizar nuevo alquiler\n");
+        printf("2. Mostrar alquileres pendientes de devolución\n");
+        printf("3. Mostrar información de un alquiler\n");
+        printf("0. Volver al menú principal\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                realizarAlquiler(archivoLectores, archivoLibros, archivoAlquileres, &listaDeGeneros, &listaDeLectores, &listaDeAlquileres);
+                break;
+            case 2:
+                mostrarAlquilerPendienteDeDevolucion(listaDeAlquileres, "nombreLectorAbuscar"); // Es para probar, aca debemos pedir al usuario que ingrese el nombre
+                break;
+            case 3:
+                mostrarDatosAlquiler(listaDeAlquileres);
+                break;
+            case 0:
+                printf("Volviendo al menú principal.\n");
+                break;
+            default:
+                printf("Opción no válida. Intente nuevamente.\n");
+                break;
+        }
+
+    } while (opcion != 0);
+}
 
 
 ///Funcion Dias
